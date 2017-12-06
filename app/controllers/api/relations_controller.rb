94e7ea51527eecaab9ba2e82ee599a08ea258cf1
Hiroki Takano <takano@hitokuse.com>
2017-12-06 10:08:35 +0900

    unique relation validation

--- app/controllers/api/relations_controller.rb
+++ app/controllers/api/relations_controller.rb
@@ -1,11 +1,14 @@
 class Api::RelationsController < ApplicationController
   def create
-    relation = Relation.create(
+    relation = Relation.where(
       user_id: current_user.id,
       friend_id: params[:user_id],
-      event_id: params[:event_id],
-      status: 1
-    )
+      event_id: params[:event_id]
+    ).first_or_initialize
+
+    relation.status = 0
+    relation.save
+
     render json: relation
   end
 end
